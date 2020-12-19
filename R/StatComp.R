8cc5c3f8aa4fc9cbb3585cce2680a4a62bb8b7d7
CuiWeijie <can@mail.ustc.edu.cn>
2020-12-19 16:05:58 +0800

    test3

--- R/StatComp.R
+++ R/StatComp.R
@@ -24,3 +24,68 @@ NULL
 sumtwo <- function(x,y) {
   return(x+y)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Title
+#'
+#' @param w wight vector
+#' @param b bias
+#' @param l_rate learning rate
+#' @param feature characteristic matrix
+#' @param target category
+#'
+#' @return parameters of hyperplane
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(StatComp20042)
+#' w = c(0,0)
+#' b=0
+#' f = iris_trim[,c(1,2)]
+#' t = iris_trim[,3]
+#' l_rate =0.001
+#' perceptronR(w,b,l_rate,f,t)
+#' }
+perceptronR<- function(w,b,l_rate,feature,target){
+  judge=TRUE
+  while(judge){
+    wrong_cnt <- 0
+    for(i in 1:nrow(feature)){
+      if(target[i] * ((feature[i,] %*% w)+b) <=0) {
+        w <- w + l_rate*target[i]*feature[i,]
+        b <- b + l_rate*target[i]
+        wrong_cnt = wrong_cnt+1
+      }
+    }
+    if (wrong_cnt ==0){
+      judge=FALSE
+    }
+
+  }
+  re<- list(w,b)
+  return(re)
+}
+
+
+
+
+
+
+
+
+
+
+
