37decf19be8875de4c166ec792e45ff19ac45288
CuiWeijie <can@mail.ustc.edu.cn>
2020-12-19 15:46:36 +0800

    test 2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,20 +5,24 @@
 
 using namespace Rcpp;
 
-// timesTwo
-int timesTwo(int x);
-RcppExport SEXP _StatComp20042_timesTwo(SEXP xSEXP) {
+// perceptronC
+NumericVector perceptronC(NumericVector w, double b, double l_rate, NumericMatrix feature, NumericVector target);
+RcppExport SEXP _StatComp20042_perceptronC(SEXP wSEXP, SEXP bSEXP, SEXP l_rateSEXP, SEXP featureSEXP, SEXP targetSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< int >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(timesTwo(x));
+    Rcpp::traits::input_parameter< NumericVector >::type w(wSEXP);
+    Rcpp::traits::input_parameter< double >::type b(bSEXP);
+    Rcpp::traits::input_parameter< double >::type l_rate(l_rateSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type feature(featureSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type target(targetSEXP);
+    rcpp_result_gen = Rcpp::wrap(perceptronC(w, b, l_rate, feature, target));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_StatComp20042_timesTwo", (DL_FUNC) &_StatComp20042_timesTwo, 1},
+    {"_StatComp20042_perceptronC", (DL_FUNC) &_StatComp20042_perceptronC, 5},
     {NULL, NULL, 0}
 };
 
--- src/code.cpp
+++ src/code.cpp
@@ -1,16 +1,56 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-//' @title timesTwo
-//' @description A Gibbs sampler using Rcpp
-//' @param x the number of between-sample random numbers
-//' @return a random sample of size a
+double dot(NumericVector x0,NumericVector y0){
+  NumericVector dotVector =x0*y0;
+  double s = 0;
+  for (int i=0;i<dotVector.length();i++){
+    s = s+dotVector[i];
+  }
+  return(s);
+}
+
+
+
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' @title perceptronC
+//' @description  divide linearly separable datasets into two categories
+//' @param w wight vector
+//' @param b bias
+//' @param l_rate learning rate
+//' @param feature characteristic matrix
+//' @param target category
+//' @return  parameters of hyperplane
 //' @examples
 //' \dontrun{
-//' timesTwo(x)
+//' library(StatComp20042)
+//' w = c(0,0)
+//' b=0
+//' f = iris_trim[,c(1,2)]
+//' t = iris_trim[,3]
+//' l_rate =0.001
+//' perceptronC(w,b,l_rate,f,t)
 //' }
 //' @export
 // [[Rcpp::export]]
-int timesTwo(int x) {
-  return x * 2;
+NumericVector perceptronC(NumericVector w,double b, double l_rate,NumericMatrix feature, NumericVector target){
+  bool judge = true;
+  while(judge){
+    int wrong_cnt =0;
+    for (int j=0;j<feature.nrow();j++){
+      if(target[j] * (dot(feature.row(j),w)+b) <= 0) {
+        w = w + l_rate*target[j]*feature.row(j);
+        b = b + l_rate*target[j];
+        wrong_cnt = wrong_cnt+1;
+      }
+    }
+    if(wrong_cnt==1){
+      judge = false;
+    }
+  }
+  w.insert(0,b);
+  return(w);
 }
