36f73e81722290fa92b3d4831ac69e7d3a43f13c
CuiWeijie <can@mail.ustc.edu.cn>
2020-12-20 16:30:13 +0800

    tr

--- R/StatComp.R
+++ R/StatComp.R
@@ -4,16 +4,12 @@ NULL
 
 
 #' @title Computing hyperplanes for binary classification problems
-#'
 #' @param w wight vector
 #' @param b bias
 #' @param l_rate learning rate
 #' @param feature character matrix
 #' @param c category
-#'
 #' @return parameters of hyperplane
-#' @export
-#'
 #' @examples
 #' \dontrun{
 #' data(iris_trim)
@@ -24,6 +20,7 @@ NULL
 #' l_rate =0.001
 #' perceptronR(w,b,l_rate,f,t)
 #' }
+#' @export
 perceptronR<- function(w,b,l_rate,feature,c){
   judge=TRUE
   while(judge){
@@ -44,34 +41,16 @@ perceptronR<- function(w,b,l_rate,feature,c){
   return(re)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #' @title Split data to training data set and test data set
-#'
 #' @param raw_data data to be splited
 #' @param train_size proportion of training set
 #' @param raw_seed random seed
-#'
 #' @return training set data and test set data
-#' @export
-#'
 #' @examples
 #' \dontrun{
 #' train_test_split(raw_data=iris_trim, train_size=0.8, raw_seed=112)
 #' }
+#' @export
 train_test_split <- function(raw_data,train_size,raw_seed){
   set.seed(raw_seed)
   sample <- sample.int(n = nrow(raw_data), size = floor(train_size*nrow(raw_data)), replace = F)
@@ -80,61 +59,48 @@ train_test_split <- function(raw_data,train_size,raw_seed){
   return(list(train,test))
 }
 
-
-
-
-
-
-#' Title
-#'
+#' @title Compute probability density function
 #' @param x a instance
 #' @param mu mean
 #' @param sigma std
-#'
 #' @return probabilty denstiny
 #' @export
-#'
 gaussian_probability <- function(x,mu,sigma){
   return(1/sqrt(2*pi)/sigma*exp(-(x-mu)^2/2/sigma^2))
 }
 
-
-
-
-
-
-
 #' @title Fit a gaussian naive bayes model.
-#'
 #' @param train A matrix composed of column vectors. The last column is the category and the former columns are features.
-#'
 #' @return A list composed of prior distribution,a matrix of mean values and a matrix of std values.
+#' @examples
+#' \dontrun{
+#' library(StatComp20042)
+#' train_and_test <- train_test_split(raw_data=iris_trim, train_size=0.8, raw_seed=112)
+#' train <- train_and_test[[1]]
+#' test <- train_and_test[[2]]
+#' gnb_list = gnb_fit(train)
+#' print(gnb_list)
+#'  }
 #' @export
-#'
 gnb_fit <- function(train) {
   train_X = train[,-ncol(train)]
   train_y = train[,ncol(train)]
   tar <- unique(train_y)
-
   prior_list <- NULL
   std_matrix <- NULL
   mean_matrix <- NULL
-
   for (i in 1:length(tar)) {
     prior <- sum(train_y==tar[i])/length(train_y)
     tem <- train_X[train_y==tar[i],]
     std_list <- apply(tem,2,function(x) sqrt(var(x)))
     mean_list <- apply(tem,2,mean)
-
     prior_list <- append(prior_list,prior)
     std_matrix <- cbind(std_matrix,std_list)
     mean_matrix <- cbind(mean_matrix,mean_list)
-
     names(tar)[i] <- paste0('category_',i)
     names(prior_list)[i] <- paste0('prior_prob_category_',i)
     colnames(std_matrix)[i] <- paste0('std_under_category_',i)
     colnames(mean_matrix)[i] <- paste0('mean_under_category_',i)
-
   }
   return(list('Category'=tar,'Prior Distribution'=prior_list,'Mean Matrix'=mean_matrix,'Std Matrix'=std_matrix))
 }
@@ -146,33 +112,38 @@ gnb_fit <- function(train) {
 #' @import stats
 #' @param test_array A feature vector to be predicted
 #' @param gnb_list the return value of gnb_fit model
-#'
 #' @return the category of test_array
+#' @examples
+#' \dontrun{
+#' library(StatComp20042)
+#' train_and_test <- train_test_split(raw_data=iris_trim, train_size=0.8, raw_seed=112)
+#' train <- train_and_test[[1]]
+#' test <- train_and_test[[2]]
+#' gnb_list = gnb_fit(train)
+#' pre_list = NULL
+#' for (i in 1:nrow(test)){
+#'   pre_list <- append(pre_list,gnb_pre(test[i,-5],gnb_list))
+#' }
+#' print(cbind(test,pre_list))
+#' }
 #' @export
-#'
 gnb_pre <- function(test_array,gnb_list){
   cat_num <- length(gnb_list$`Prior Distribution`)
-
   prob_list = numeric(cat_num)
-
   for (i in 1:cat_num) {
     prior <- gnb_list$`Prior Distribution`[i]
-
     con_prob <- 1
     for (j in 1:length(test_array)){
       tem_mean <- gnb_list$`Mean Matrix`[j,i]
       tem_std <- gnb_list$`Std Matrix`[j,i]
       con_prob <- con_prob * gaussian_probability(test_array[j],tem_mean,tem_std)
     }
-
     prob_list[i] <- prior * con_prob
   }
   index <- match(max(prob_list),prob_list)
-
   if (length(index)>1) {
     index <- index[sample(1:length(index),1)]
   }
-
   return(gnb_list$`Category`[index])
 }
 
